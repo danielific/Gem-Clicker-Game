43b62ab856e1119c140992dad3654434f030a8ee
Sinclair <51466662+danielific@users.noreply.github.com>
2020-02-26 00:39:30 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+var gameData = {          //crucial variables
+    geodeOnScreen: false,
+    dirt: 0,
+    stone: 0,
+}
+  
+    
 
+
+
+
+
+
+
+
+var mainGameLoop = window.setInterval(function() { //ticks, 1 per second.
+}, 1000)
