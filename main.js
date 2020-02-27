777ee5fee93ce3201e31be0b92bf4bbe20d4150e
Sinclair <51466662+danielific@users.noreply.github.com>
2020-02-27 00:53:23 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 var gameData = {          //crucial variables
     geodeOnScreen: false, //determines if the Geode appears on screen
-    geodeHealthPoints: 10,
+    geodeHealthPoints: 10, //how much hp the geode must lose before dissapearing from screen
     totalDirt: 0, //how much dirt the user has
     totalStone: 0, //how much stone the user has
+    clickStrength: 1, //how much damage the click does to the geode
     
 }
   let smashGeodeBtn = document.getElementById('smashGeode');
-  
-    
+  let totalDirt = document.getElementById("totalDirt");
+  let totalStone = document.getElementById("totalStone");
+  smashGeodeBtn.addEventListener('click', smashGeode);
+
+
 function smashGeode(amount){
+  
 	gameData.geodeHealthPoints -= amount;
-	geodeHealth.innerText = gameData.geodeHealthPoints - "1"}
-
-
-
-
-
+	document.getElementById("geodeHealthPoints").value -= "1";
+  }
 
 
 var mainGameLoop = window.setInterval(function() { //ticks, 1 per second.
