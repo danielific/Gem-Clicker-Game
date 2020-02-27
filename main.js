097632ae776a7465e844fe0efb8f4b823f356e0b
Sinclair <51466662+danielific@users.noreply.github.com>
2020-02-26 22:02:36 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 var gameData = {          //crucial variables
-    geodeOnScreen: false,
-    dirt: 0,
-    stone: 0,
+    geodeOnScreen: false, //determines if the Geode appears on screen
+    geodeHealthPoints: 10,
+    totalDirt: 0, //how much dirt the user has
+    totalStone: 0, //how much stone the user has
+    
 }
+  let smashGeodeBtn = document.getElementById('smashGeode');
   
     
-
+function smashGeode(amount){
+	gameData.geodeHealthPoints -= amount;
+	geodeHealth.innerText = gameData.geodeHealthPoints - "1"}
 
 
 
